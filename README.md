0659c8d1ca1ccef05653c0d02eaa310623af3a80
paulopperman <paul.opperman@gmail.com>
2017-03-12 23:24:34 -0400

    structuring some dataframes

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ accessed 3/12/2017 from [https://lihtc.huduser.gov/](https://lihtc.huduser.gov/)
 current as of 2/27/2017 from [https://portal.hud.gov/hudportal/HUD?src=/program_offices/housing/mfh/presrv/mfhpreservation](https://portal.hud.gov/hudportal/HUD?src=/program_offices/housing/mfh/presrv/mfhpreservation)
 
 
+## TODO
+Add in other relevant data, like transportation, food, services, etc.
+* [RIPTA Route Data](http://www.rigis.org/data/RIPTAroutes)
--- housing-data-processing.py
+++ housing-data-processing.py
@@ -5,6 +5,29 @@ import pandas as pd
 lihtc = pd.read_csv('./datasets/lihtc-ri-output.csv')  # ri low income housing tax credit data
 sec_eight = pd.read_csv('./datasets/ri-sec-8.csv')  # ri section 8 active properties
 
+#TODO: replace NaNs in the dataframes with ' ' to allow bug-free string building instead of ignoring address2
+
+# build address strings for lihtc table
+# extract important address strings
+lihtc_addrs_cells = [lihtc.PROJ_ADD.values.tolist(),
+                     lihtc.PROJ_CTY.values.tolist(),
+                     lihtc.PROJ_ST.values.tolist()]
+lihtc_addrs_tup = list(zip(*lihtc_addrs_cells))
+
+lihtc_addrs_str = []  #initialize list of strings
+for i in range(0, len(lihtc_addrs_tup)):
+    lihtc_addrs_str.insert(0, ' '.join(lihtc_addrs_tup.pop()))
+
+# build address strings for section eight table
+sec_eight_addrs_cells = [sec_eight.address_line1_text.values.tolist(),
+                         sec_eight.city_name_text.values.tolist(),
+                         sec_eight.state_code.values.tolist()]
+sec_eight_addrs_tup = list(zip(*sec_eight_addrs_cells))
+
+sec_eight_addrs_str = []
+for k in range(0, len(sec_eight_addrs_tup)):
+    sec_eight_addrs_str.insert(0, ' '.join(sec_eight_addrs_tup.pop()))
 
 
-
+# TODO: merge strings with property names
+# TODO: code by number of units
